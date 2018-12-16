36edc0470e45b02621d33a2cd7113d50f73aa44a
Darth-Par <pmicoo@hotmail.com>
2018-12-16 22:25:26 +0000

    Static assets setup.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 // App and port declarations.  Instance of express and port.
 const app = express();
@@ -10,10 +11,17 @@ const port = 8000
 
 // Middleware.
 app.use(morgan('combined'));
+// Assets served from public folder.
+app.use(express.static(path.join(__dirname, '/public/')));
+// Bootstrap, jQuery, Popper.js served from node_modules
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
 
 // GET route '/'.
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 // Server running...
