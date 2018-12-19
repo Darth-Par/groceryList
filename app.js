79e63947547067a28deb74c87f44805b034ebd46
Darth-Par <pmicoo@hotmail.com>
2018-12-19 02:10:29 +0000

    Linting and scripting.

--- app.js
+++ app.js
@@ -7,24 +7,25 @@ const path = require('path');
 
 // App and port declarations.  Instance of express and port.
 const app = express();
-const port = 8000
+const port = 8000;
 
 // Middleware.
 app.use(morgan('combined'));
 // Assets served from public folder.
 app.use(express.static(path.join(__dirname, '/public/')));
-// Bootstrap, jQuery, Popper.js served from node_modules
+// Bootstrap, jQuery, Popper.js and font-awesome served from node_modules
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/css')));
 
 // GET route '/'.
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 // Server running...
 app.listen(port, () => {
-    debug(`Server listening on port ${chalk.blue(port)}!`);
+  debug(`Server listening on port ${chalk.blue(port)}!`);
 });
